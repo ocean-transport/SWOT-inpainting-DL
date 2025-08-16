3fddd8391553a852f71fefbd3f061b56e5627eef
Tatsu Monkman <tm3076@cs649.hpc.nyu.edu>
2025-08-16 11:53:38 -0400

    add some redundancy for switching .zarr stores

--- src/claude_data_loaders.py
+++ src/claude_data_loaders.py
@@ -143,12 +143,12 @@ class llc4320_dataset(Dataset):
 
     def __len__(self):
         return self.patch_coords.shape[0]
-
+    
     def __getitem__(self, idx):
-        pid = str(int(self.patch_coords[idx,2])).zfill(3)
+        pid = str(int(self.patch_coords[idx, 2])).zfill(3)
         coords = self.patch_coords[idx]
         meta = {"patch_ID": pid, "patch_coords": coords, "mid_timestep": self.mid_timestep}
-        if getattr(_thread_local, 'initialized', False) is False:
+        if not getattr(_thread_local, 'initialized', False):
             self._init_worker_local()
         invar, inmask = self._load_fields(pid, self.infields, self.in_transform_list, self.in_mask_list)
         if self.outfields:
@@ -159,13 +159,14 @@ class llc4320_dataset(Dataset):
         if self.squeeze:
             invar, outvar = invar.squeeze(), outvar.squeeze()
             inmask, outmask = inmask.squeeze(), outmask.squeeze()
-        if self.return_meta_data and self.return_masks:
-            return invar, outvar, inmask, outmask, meta
-        elif self.return_meta_data:
-            return invar, outvar, meta
-        elif self.return_masks:
-            return invar, outvar, inmask, outmask
-        return invar, outvar
+        result = [invar, outvar]
+        if self.return_masks:
+            result.extend([inmask, outmask])
+        if self.return_meta_data:
+            result.append(meta)
+        if len(result) == 1:
+            return result[0]
+        return tuple(result)
 
     def _init_worker_local(self):
         """Initialize per-worker resources with optimizations"""
@@ -231,9 +232,13 @@ class llc4320_dataset(Dataset):
         vars, masks = [], []
         for fld, tk, mask_key in zip(fields, tkeys, mask_keys):
             # Use cached zarr opening
-            path = f"{self.data_dir}/{fld}_allpatches.zarr"
-            ds = self._get_cached_zarr_dataset(path, threading.get_ident())
-            d = ds.loc[{"patch": int(pid)}]
+            if os.path.exists(f"{self.data_dir}/{fld}_allpatches.zarr"):
+                path = f"{self.data_dir}/{fld}_allpatches.zarr"
+                ds = self._get_cached_zarr_dataset(path, threading.get_ident())
+                d = ds.loc[{"patch": int(pid)}]
+            else:
+                path = f"{self.data_dir}/{fld}/{pid:03}.zarr"
+                d = self._get_cached_zarr_dataset(path, threading.get_ident())
             d = d.isel(time=slice(self.mid_timestep - self.N_t//2,
                                   self.mid_timestep + self.N_t//2 + self.N_t%2))
             if isinstance(d, xr.Dataset):
@@ -371,3 +376,5 @@ class llc4320_dataset(Dataset):
         # Optimized mask computation
         mask_values = np.where(sliced.values > 0, 1.0, 0.0).astype(np.float32)
         return torch.from_numpy(mask_values)
+
+
